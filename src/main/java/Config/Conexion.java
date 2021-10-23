9192121dc8b27a498789e1d4be57932ad6122f42
Jefferson-PC <Jefferson-PC@Jefferson-PC>
2021-10-23 13:23:12 -0500

    CAMBIO MARIA DB

--- src/main/java/Config/Conexion.java
+++ src/main/java/Config/Conexion.java
@@ -4,16 +4,16 @@ import java.sql.*;
 
 public class Conexion {
 	
-	private String bd = "tiendagenerica";
-	private String url = "jdbc:mysql://localhost:3306/"+bd;
-	private String usuario = "root";
-	private String pass = "";
+	private String bd = "grupo25_equipo6";
+	private String url = "jdbc:mariadb://grupo25db2.c47knbsonjdi.us-east-2.rds.amazonaws.com/"+bd;
+	private String usuario = "admin";
+	private String pass = "Grupo25DB2";
 	
 	Connection conec = null;
 	
 	public Connection conectar() {
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
+			Class.forName("org.mariadb.jdbc.Driver");
 			conec = DriverManager.getConnection(url,usuario,pass);
 			//JOptionPane.showMessageDialog(null, "Conexion OK....");
 		} catch (Exception e) {
